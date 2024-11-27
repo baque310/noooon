60ab29c59fc49f5b4fadd0ebd29b7ae9f47fca9a
Abbas Alsbihawi <43404498+AbbasAlsbihawi@users.noreply.github.com>
2024-11-27 19:56:48 +0300

    refactor: consolidate logic into PageComponent and remove unused logic files

--- src/app/(defaults)/(admin)/section/[id]/_components/PageComponent.tsx
+++ src/app/(defaults)/(admin)/section/[id]/_components/PageComponent.tsx
@@ -3,16 +3,47 @@ import React, { useState } from 'react';
 import { LoadingForm } from '@/components/Form/loadingForm';
 import { BackButton } from '@/components/common/BackButton';
 import { ItemList } from '@/components/common/ItemList';
-
-
-import useLogic from './_logic';
+import { getTranslation } from "@/ni18n/i18n";
+import { useLazySectionGetDataByIdQuery, useSectionRemoveMutation } from "@/services/admin/section";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 import { ArrowIcons } from '@/components/common/icons/Actions';
 import moment from 'moment';
-import CreateComponent from '../../_components/create/_components/CreateComponent';
+import CreateComponent from '../../_components/CreateComponent';
 import DeleteModel from '@/components/Model/DeleteModel';
 
 const PageComponent = () => {
-    const { t, data, isFetching, handleRemove, isLoadingSectionRemove } = useLogic();
+    const { t } = getTranslation();
+    const router = useRouter()
+    const params = useParams()
+    const { id } = params
+    const [SectionGetDataById, { currentData: data, isFetching }] = useLazySectionGetDataByIdQuery()
+    const [SectionRemove, { isLoading: isLoadingSectionRemove }] = useSectionRemoveMutation()
+    useEffect(() => {
+        if (id) {
+            SectionGetDataById({ id: String(id) })
+                .then((data) => {
+                    if (!data.data) {
+                        router.back();
+                    }
+                });
+        }
+    }, [id])
+
+    const handleRemove = async () => {
+        try {
+            await SectionRemove({ id: String(id) }).unwrap();
+            toast.success(t('common.deleted-successfully'), { autoClose: 15000 });
+            router.back();
+        } catch (error: any) {
+            console.error('Failed to operation :', error);
+            if (error && error.message) {
+                return toast.error(error.message, { autoClose: 15000 });
+            }
+            toast.error(error, { autoClose: 15000 });
+        }
+    };
     const [open, setOpen] = useState(false)
     const [openDelete, setOpenDelete] = useState(false)
     return (
--- src/app/(defaults)/(admin)/section/_components/Table.tsx
+++ src/app/(defaults)/(admin)/section/_components/Table.tsx
@@ -1,32 +1,81 @@
 "use client";
 import { DataTable } from "mantine-datatable";
 import React, { useState } from "react";
-
- import { RolePageAndActionBasedComponent, withRole } from "@/components/Provider/RolePageAndActionBasedComponent";
-import useLogic from "./_logic";
-import { AddIcons } from "@/components/common/icons/Actions";
-import CreateComponent from "./create/_components/CreateComponent";
+import { RolePageAndActionBasedComponent, withRole } from "@/components/Provider/RolePageAndActionBasedComponent";
+ import { AddIcons } from "@/components/common/icons/Actions";
+import CreateComponent from "./CreateComponent";
 import { SelectWithSearch } from "@/components/Filter/SelectSearch";
+import useMounted from "@/hooks/useMounted";
+import { getTranslation } from "@/ni18n/i18n";
+import { useClassGetDataQuery } from "@/services/admin/class";
+import { useSectionGetDataQuery } from "@/services/admin/section";
+import { IRootState } from "@/store";
+import { DataTableSortStatus } from "mantine-datatable";
+import { useRouter, useSearchParams } from "next/navigation";
+ import { useSelector } from "react-redux";
+
 
 
 const TableComponent = () => {
-  const {
-    t,
-    data,
-    isFetching,
-    router,
-    isMounted,
-    isDark,
-    Search,
-    handleKeyPress,
-    handleChange,
-    handleSelectClass,
-    isFetchingClassData,
-    ClassData,
-    sortStatus,
-    setSortStatus,
+  const { t } = getTranslation();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") || "";
+  const [sortStatus, setSortStatus] = useState<DataTableSortStatus>({
+    columnAccessor: "createdAt",
+    direction: "desc",
+  });
 
-  } = useLogic()
+  const [param, setParam] = useState<
+    | {
+      search?: string;
+      classId?: string;
+    }
+    | undefined
+  >();
+  const params = {
+    sortBy: sortStatus.columnAccessor,
+    sortDirection: sortStatus.direction,
+    ...(search && { search: search as string }),
+    ...param,
+  };
+
+  const isDark = useSelector((state: IRootState) => state.themeConfig.theme) === "dark";
+  const { isMounted } = useMounted();
+  const { isFetching, currentData: data } = useSectionGetDataQuery(params);
+  const { isFetching: isFetchingClassData, currentData: ClassData } = useClassGetDataQuery({});
+
+
+  const [Search, setSearch] = useState(search);
+  const handleChange = (e: any) => {
+    const value = e.target.value;
+    setSearch(value);
+    if (value == "") {
+      handleSearch(value);
+    }
+  };
+  const allParams = new URLSearchParams(searchParams);
+  const handleSearch = (value?: string) => {
+    if (search != Search) {
+      allParams.set("search", value ?? Search);
+      router.push(`/section?${allParams.toString()}`);
+    }
+  };
+  const handleKeyPress = (event: any) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  const handleSelectClass = (value: any) => {
+    if (value) {
+      setParam({ ...param, classId: value.value });
+
+    } else {
+      setParam({ ...param, classId: undefined });
+
+    }
+  }
   const [open, setOpen] = useState(false)
   return (
     <div className={`m-4 rtl:transition-[left] ltr:transition-[right] duration-1000`}>
