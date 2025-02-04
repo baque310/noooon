255cf616d2eb85e40030ed69b1e559437d9a7059
Abbas Alsbihawi <abbas202559@gmail.com>
2025-02-05 00:08:00 +0300

    feat: add score validation messages and update exam interfaces to include score property

--- src/services/admin/Exams.ts
+++ src/services/admin/Exams.ts
@@ -6,117 +6,131 @@ import { ISchoolYear } from "../SchoolYear";
 import { ISection } from "./section";
 
 export interface IExams {
-    "id": string
-    "content": string
-    "createdAt": string
-    "updatedAt": string
-    "stageSubjectId": string
-    "examTypeId": string
-    "schoolYearId": string
-    "ExamType": IExamType
-    "ExamSection": {
-        "id": string
-        "examDate": string
-        "Section": ISection
-    }[],
-    "StageSubject": IStageSubject
-    "SchoolYear": ISchoolYear
-
-
+  id: string;
+  content: string;
+  score: number;
+  createdAt: string;
+  updatedAt: string;
+  stageSubjectId: string;
+  examTypeId: string;
+  schoolYearId: string;
+  ExamType: IExamType;
+  ExamSection: {
+    id: string;
+    examDate: string;
+    Section: ISection;
+  }[];
+  StageSubject: IStageSubject;
+  SchoolYear: ISchoolYear;
 }
 
 export interface AddExamsPayload {
-    content: string,
-    stageSubjectId: string
-    examTypeId: string
-    ExamSection: {
-        examDate: string
-        sectionId: string
-    }[]
+  content: string;
+  stageSubjectId: string;
+  examTypeId: string;
+  score: number;
+  ExamSection: {
+    examDate: string;
+    sectionId: string;
+  }[];
 }
 export interface UpdateExamsPayload {
-    content: string,
+  content: string;
 }
 
-
 export interface AddExamsCreateSection {
-    "examDate": string
-    "sectionId": string
+  examDate: string;
+  sectionId: string;
+  score: number;
 }
 
 export const Exams = api.injectEndpoints({
-    endpoints: (build) => ({
-        ExamsGetData: build.query<BaseGetDataResponse<IExams[]>, GetDataRequestParams>({
-            query: (params) => ({
-                url: `admin/exams`,
-                params,
-                method: "GET",
-            }),
-            providesTags: ["ExamsGetData"],
-
-        }),
-
-        ExamsGetDataById: build.query<IExams, { id: string }>({
-            query: ({ id }) => ({
-                url: `admin/exams/${id}`,
-                method: "GET",
-            }),
-            providesTags: ["ExamsGetDataById"],
-        }),
-
-        ExamsCreate: build.mutation<IExams, AddExamsPayload>({
-            query: (body) => ({
-                url: `admin/exams`,
-                body,
-                method: "POST",
-            }),
-            invalidatesTags: ["ExamsCreate", "ExamsGetDataById", "ExamsGetData"],
-        }),
-        ExamsCreateSections: build.mutation<IExams, { id: string, body: AddExamsCreateSection }>({
-            query: ({ body, id }) => ({
-                url: `admin/exams/${id}/sections`,
-                body,
-                method: "POST",
-            }),
-            invalidatesTags: ["ExamsCreateSections", "ExamsGetDataById", "ExamsGetData"],
-        }),
-
-        ExamsUpdate: build.mutation<IExams, { id: string, body: UpdateExamsPayload }>({
-            query: ({ body, id }) => ({
-                url: `admin/exams/${id}`,
-                body,
-                method: "PATCH",
-            }),
-            invalidatesTags: ["ExamsUpdate", "ExamsGetDataById", "ExamsGetData"],
-        }),
-
-        ExamsRemove: build.mutation<void, { id: string }>({
-            query: ({ id }) => ({
-                url: `admin/exams/${id}`,
-                method: "DELETE",
-            }),
-            invalidatesTags: ["ExamsRemove", "ExamsGetDataById", "ExamsGetData"],
-        }),
-        ExamSectionsRemove: build.mutation<void, { examSectionIds: string[] }>({
-            query: (body) => ({
-                url: `admin/exams/examSections`,
-                method: "DELETE",
-                body
-            }),
-            invalidatesTags: ["ExamSectionsRemove", "ExamsGetDataById", "ExamsGetData"],
-        }),
-
+  endpoints: (build) => ({
+    ExamsGetData: build.query<
+      BaseGetDataResponse<IExams[]>,
+      GetDataRequestParams
+    >({
+      query: (params) => ({
+        url: `admin/exams`,
+        params,
+        method: "GET",
+      }),
+      providesTags: ["ExamsGetData"],
     }),
+
+    ExamsGetDataById: build.query<IExams, { id: string }>({
+      query: ({ id }) => ({
+        url: `admin/exams/${id}`,
+        method: "GET",
+      }),
+      providesTags: ["ExamsGetDataById"],
+    }),
+
+    ExamsCreate: build.mutation<IExams, AddExamsPayload>({
+      query: (body) => ({
+        url: `admin/exams`,
+        body,
+        method: "POST",
+      }),
+      invalidatesTags: ["ExamsCreate", "ExamsGetDataById", "ExamsGetData"],
+    }),
+    ExamsCreateSections: build.mutation<
+      IExams,
+      { id: string; body: AddExamsCreateSection }
+    >({
+      query: ({ body, id }) => ({
+        url: `admin/exams/${id}/sections`,
+        body,
+        method: "POST",
+      }),
+      invalidatesTags: [
+        "ExamsCreateSections",
+        "ExamsGetDataById",
+        "ExamsGetData",
+      ],
+    }),
+
+    ExamsUpdate: build.mutation<
+      IExams,
+      { id: string; body: UpdateExamsPayload }
+    >({
+      query: ({ body, id }) => ({
+        url: `admin/exams/${id}`,
+        body,
+        method: "PATCH",
+      }),
+      invalidatesTags: ["ExamsUpdate", "ExamsGetDataById", "ExamsGetData"],
+    }),
+
+    ExamsRemove: build.mutation<void, { id: string }>({
+      query: ({ id }) => ({
+        url: `admin/exams/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["ExamsRemove", "ExamsGetDataById", "ExamsGetData"],
+    }),
+    ExamSectionsRemove: build.mutation<void, { examSectionIds: string[] }>({
+      query: (body) => ({
+        url: `admin/exams/examSections`,
+        method: "DELETE",
+        body,
+      }),
+      invalidatesTags: [
+        "ExamSectionsRemove",
+        "ExamsGetDataById",
+        "ExamsGetData",
+      ],
+    }),
+  }),
 });
 export const {
-    useExamsGetDataQuery,
-    useLazyExamsGetDataQuery,
-    useExamsGetDataByIdQuery,
-    useLazyExamsGetDataByIdQuery,
-    useExamsCreateMutation,
-    useExamsRemoveMutation,
-    useExamsUpdateMutation,
-    useExamsCreateSectionsMutation,
-    useExamSectionsRemoveMutation
-
+  useExamsGetDataQuery,
+  useLazyExamsGetDataQuery,
+  useExamsGetDataByIdQuery,
+  useLazyExamsGetDataByIdQuery,
+  useExamsCreateMutation,
+  useExamsRemoveMutation,
+  useExamsUpdateMutation,
+  useExamsCreateSectionsMutation,
+  useExamSectionsRemoveMutation,
 } = Exams;
