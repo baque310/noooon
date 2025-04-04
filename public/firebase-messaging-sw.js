a653efab433be1a33062d815d7d28dcf101b2112
Abbas Alsbihawi <abbas202559@gmail.com>
2025-04-04 11:33:13 +0300

    feat: update Firebase configuration with production credentials

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,13 +6,13 @@ importScripts(
 );
 
 firebase.initializeApp({
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId:"",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyAGXMQK5LxWMy8VHh-LOx2Kxf6Lskrim6M",
+  authDomain: "noon-f0d05.firebaseapp.com",
+  projectId: "noon-f0d05",
+  storageBucket: "noon-f0d05.firebasestorage.app",
+  messagingSenderId: "486586508241",
+  appId: "1:486586508241:web:05d97edd2369596ae4b6f0",
+  measurementId: "G-VMVJS5KFT2"
 });
 
 const messaging = firebase.messaging();
