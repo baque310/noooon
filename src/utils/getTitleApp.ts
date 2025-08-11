8c5d115b5291a2b15bd58b22c273282f784e42a7
Abbas Alsbihawi <43404498+AbbasAlsbihawi@users.noreply.github.com>
2025-08-11 22:50:29 +0300

    fix(getTitleApp): update title checks to include full dashboard identifiers

--- src/utils/getTitleApp.ts
+++ src/utils/getTitleApp.ts
@@ -1,11 +1,11 @@
 export const getTitleApp = (title: string) => {
   console.log("Getting title for:", title);
 
-  if (title.includes("noon-iraq")) {
+  if (title.includes("dashboard.noon-iraq")) {
     return "Noon Iraq";
-  } else if (title.includes("alghad")) {
+  } else if (title.includes("alghad-dashboard")) {
     return "Alghad";
-  } else if (title.includes("almalak")) {
+  } else if (title.includes("almalak-dashboard")) {
     return "Almalak";
   } else {
     return "Noon Iraq";
