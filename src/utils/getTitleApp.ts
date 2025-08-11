4cd87f29c14a6a524a41dac18e0acc7d5113d4b3
Abbas Alsbihawi <43404498+AbbasAlsbihawi@users.noreply.github.com>
2025-08-12 00:15:24 +0300

    fix(getTitleApp): remove console log and streamline title checks for dashboard

--- src/utils/getTitleApp.ts
+++ src/utils/getTitleApp.ts
@@ -1,9 +1,5 @@
 export const getTitleApp = (title: string) => {
-  console.log("Getting title for:", title);
-
-  if (title.includes("dashboard.noon-iraq")) {
-    return "Noon Iraq";
-  } else if (title.includes("alghad-dashboard")) {
+  if (title.includes("alghad-dashboard")) {
     return "Alghad";
   } else if (title.includes("almalak-dashboard")) {
     return "Almalak";
