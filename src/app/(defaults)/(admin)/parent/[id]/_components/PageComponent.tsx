d414210c101ccb3e2a304eb0c1043d664385d79b
Abbas Alsbihawi <43404498+AbbasAlsbihawi@users.noreply.github.com>
2025-08-21 14:12:39 +0300

    delete Parent

--- src/app/(defaults)/(admin)/parent/[id]/_components/PageComponent.tsx
+++ src/app/(defaults)/(admin)/parent/[id]/_components/PageComponent.tsx
@@ -119,6 +119,15 @@ const PageComponent = () => {
       router.back();
     } catch (error: any) {
       console.error("Failed to operation :", error);
+      if (
+        error &&
+        error.message ==
+          `Foreign key constraint failed on the field. More details: {"modelName":"User","field_name":"userId"}`
+      ) {
+        return toast.error(t("Cannot delete user: User has related records"), {
+          autoClose: 15000,
+        });
+      }
       if (error && error.message) {
         return toast.error(t(error.message), { autoClose: 15000 });
       }
@@ -193,17 +202,15 @@ const PageComponent = () => {
               title={t("ParentPage.update-info")}
               value={<ArrowIcons className="rtl:rotate-180 text-[#000]/50" />}
             />
-            {/* <ItemList
-                            props={{
-                                onClick: () => {
-                                    setOpenDelete(true)
-                                }
-                            }}
-                            title={<div className='text-danger'>
-                                {t('common.delete')}
-                            </div>}
-                            value={<ArrowIcons className='rtl:rotate-180 text-danger/50' />}
-                        /> */}
+            <ItemList
+              props={{
+                onClick: () => {
+                  setOpenDelete(true);
+                },
+              }}
+              title={<div className="text-danger">{t("common.delete")}</div>}
+              value={<ArrowIcons className="rtl:rotate-180 text-danger/50" />}
+            />
           </div>
         </>
       )}
