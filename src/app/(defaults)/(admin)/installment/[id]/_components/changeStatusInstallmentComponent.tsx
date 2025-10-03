ae2b81988d7e6cdf1ae6c0379c83d936456e3d64
ahmed abass <ahmedabasss1515@gmail.com>
2025-10-04 02:07:34 +0300

    update

--- src/app/(defaults)/(admin)/installment/[id]/_components/changeStatusInstallmentComponent.tsx
+++ src/app/(defaults)/(admin)/installment/[id]/_components/changeStatusInstallmentComponent.tsx
@@ -13,36 +13,58 @@ import * as Yup from "yup";
 import {
   IInstallmentPayments,
   InstallmentPaymentStatusPayload,
+  PaymentMethod,
+  Status,
+  useInstallmentPaymentCreateMutation,
   useInstallmentPaymentUpdateStatusMutation,
 } from "@/services/admin/installmentPayment";
 import { SelectForm } from "@/components/Form/SelectForm";
-export interface FormValues extends InstallmentPaymentStatusPayload {}
+import { InputForm } from "@/components/Form/inputForm";
+
+// Correct form values type
+export interface FormValues extends InstallmentPaymentStatusPayload {
+  status: Status;
+}
+
 const ChangeStatusInstallmentComponent = ({
   open,
   setOpen,
   data,
+  outstandingAmount,
 }: {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
   data: IInstallmentPayments;
+  outstandingAmount: number | undefined;
 }) => {
   const { t } = getTranslation();
 
-  const [
-    InstallmentPaymentUpdate,
-    { isLoading: isLoadingInstallmentPaymentUpdate },
-  ] = useInstallmentPaymentUpdateStatusMutation();
+  const [InstallmentPaymentUpdate, { isLoading: isLoadingInstallmentPaymentUpdate }] = useInstallmentPaymentUpdateStatusMutation();
+  const [InstallmentPaymentCreate, { isLoading: isLoadingInstallmentPaymentCreate }] = useInstallmentPaymentCreateMutation();
 
-  const handleSubmit = async (
-    values: FormValues,
-    { setSubmitting, resetForm }: FormikHelpers<FormValues>,
-    setOpen: any
-  ) => {
+  const handleSubmit = async (values: FormValues, { setSubmitting, resetForm }: FormikHelpers<FormValues>, setOpen: any) => {
     try {
-      await InstallmentPaymentUpdate({
-        id: data.id,
-        status: values.status!,
-      }).unwrap();
+      if (data?.isPaid === "partial") {
+        await InstallmentPaymentCreate({
+          id: data.installmentId,
+          body: {
+            paidAmount: Number(values.paidAmount),
+            paymentMethod: values.paymentMethod,
+            notes: values.notes,
+          },
+        });
+      } else {
+        console.log("other");
+        await InstallmentPaymentUpdate({
+          id: data.id,
+          status: values.status,
+          body: {
+            paidAmount: Number(values.paidAmount),
+            paymentMethod: values.paymentMethod,
+            notes: values.notes,
+          },
+        }).unwrap();
+      }
 
       toast.success(t("common.changeStatus-successfully"), {
         autoClose: 30000,
@@ -51,22 +73,18 @@ const ChangeStatusInstallmentComponent = ({
       setOpen(false);
     } catch (error: any) {
       console.error("Failed to operation :", error);
-      if (error) {
-        if (error.message == "name already exist") {
-          return toast.error(t("ClassPage.name-already-exists"), {
-            autoClose: 30000,
-          });
-        }
-
-        return toast.error(JSON.stringify(error), { autoClose: 30000 });
+      if (error?.message === "name already exist") {
+        return toast.error(t("ClassPage.name-already-exists"), {
+          autoClose: 30000,
+        });
       }
-      toast.error(error, { autoClose: 30000 });
+      toast.error(JSON.stringify(error), { autoClose: 30000 });
     }
   };
+
+  // Yup schema with enum validation
   const schema = Yup.object().shape({
-    status: Yup.string()
-      .oneOf(["paid", "unpaid"])
-      .required(t("common.this-field-is-required")),
+    status: Yup.mixed<Status>().oneOf([Status.Paid, Status.Unpaid, Status.Partial]).required(t("common.this-field-is-required")),
   });
 
   return (
@@ -76,29 +94,48 @@ const ChangeStatusInstallmentComponent = ({
       ) : (
         <Formik<FormValues>
           initialValues={{
-            status: data?.isPaid ?? "unpaid",
+            status: data?.isPaid ?? Status.Unpaid,
+            paidAmount: data?.isPaid === "partial" ? outstandingAmount : data?.amount ?? 0,
+            paymentMethod: data?.paymentMethod ?? "",
+            notes: data?.notes ?? "",
           }}
           validationSchema={schema}
           onSubmit={(values, formikHelpers) => {
             handleSubmit(values, formikHelpers, setOpen);
-          }}
-        >
-          {(props: FormikProps<any>) => (
-            <Form className={"flex flex-col gap-4"}>
+          }}>
+          {(props: FormikProps<FormValues>) => (
+            <Form className="flex flex-col gap-4">
+              {data?.isPaid !== "partial" && (
+                <SelectForm
+                  formikProps={props}
+                  name="status"
+                  title={t("InstallmentPage.status")}
+                  placeholder={t("InstallmentPage.enter-status")}
+                  options={[
+                    { label: t("unpaid"), value: Status.Unpaid },
+                    { label: t("paid"), value: Status.Paid },
+                    { label: t("partial"), value: Status.Partial },
+                  ]}
+                  props={{
+                    isClearable: true,
+                    onChange: (e: any) => {
+                      props.setFieldValue("status", e?.value ?? "");
+                    },
+                  }}
+                />
+              )}
+              {props.values.status === Status.Partial && (
+                <InputForm formikProps={props} name={"paidAmount"} title={t("BusPage.paidAmount")} placeholder={t("BusPage.enter-paidAmount")} />
+              )}
               <SelectForm
                 formikProps={props}
-                name={"status"}
-                title={t("InstallmentPage.status")}
-                placeholder={t("InstallmentPage.enter-status")}
+                name="paymentMethod"
+                title={t("InstallmentPage.paymentMethod")}
+                placeholder={t("InstallmentPage.enter-paymentMethod")}
                 options={[
-                  {
-                    label: t("unpaid"),
-                    value: "unpaid",
-                  },
-                  {
-                    label: t("paid"),
-                    value: "paid",
-                  },
+                  { label: t("Cash"), value: PaymentMethod.Cash },
+                  { label: t("ZainCash"), value: PaymentMethod.ZainCash },
+                  { label: t("QiCard"), value: PaymentMethod.QiCard },
                 ]}
                 props={{
                   isClearable: true,
@@ -108,6 +145,9 @@ const ChangeStatusInstallmentComponent = ({
                 }}
               />
 
+              {/* <InputForm formikProps={props} name={"paymentMethod"} title={t("BusPage.paymentMethod")} placeholder={t("BusPage.enter-paymentMethod")} /> */}
+              <InputForm formikProps={props} name={"notes"} title={t("BusPage.notes")} placeholder={t("BusPage.enter-notes")} />
+
               <div className="flex flex-row-reverse gap-2">
                 <ButtonForm
                   props={{
@@ -115,7 +155,7 @@ const ChangeStatusInstallmentComponent = ({
                     className: `w-full`,
                   }}
                   title={t("common.save")}
-                  isLoading={isLoadingInstallmentPaymentUpdate}
+                  isLoading={isLoadingInstallmentPaymentUpdate || isLoadingInstallmentPaymentCreate}
                 />
               </div>
             </Form>
--- src/app/(defaults)/(admin)/studentEnrollment/createOrUpdate/_components/PageComponent.tsx
+++ src/app/(defaults)/(admin)/studentEnrollment/createOrUpdate/_components/PageComponent.tsx
@@ -6,11 +6,7 @@ import { BackButton } from "@/components/common/BackButton";
 import { Form, Formik, FormikProps } from "formik";
 import { ButtonForm } from "@/components/Form/ButtonForm";
 import { getTranslation } from "@/ni18n/i18n";
-import {
-  AddStudentEnrollmentPayload,
-  useLazyStudentEnrollmentGetDataByIdQuery,
-  useStudentEnrollmentCreateMutation,
-} from "@/services/admin/studentEnrollment";
+import { AddStudentEnrollmentPayload, useLazyStudentEnrollmentGetDataByIdQuery, useStudentEnrollmentCreateMutation } from "@/services/admin/studentEnrollment";
 import { FormikHelpers } from "formik";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -35,8 +31,7 @@ const PageComponent = () => {
   const searchParams = useSearchParams();
   const id = searchParams.get("id");
   const [searchStudent, setSearchStudent] = useState("");
-  const [StudentEnrollmentGetDataById, { currentData: data, isFetching }] =
-    useLazyStudentEnrollmentGetDataByIdQuery();
+  const [StudentEnrollmentGetDataById, { currentData: data, isFetching }] = useLazyStudentEnrollmentGetDataByIdQuery();
   useEffect(() => {
     if (id) {
       StudentEnrollmentGetDataById({ id: String(id) }).then((data) => {
@@ -47,28 +42,18 @@ const PageComponent = () => {
     }
   }, [id]);
 
-  const [
-    StudentEnrollmentCreate,
-    { isLoading: isLoadingStudentEnrollmentCreate },
-  ] = useStudentEnrollmentCreateMutation();
+  const [StudentEnrollmentCreate, { isLoading: isLoadingStudentEnrollmentCreate }] = useStudentEnrollmentCreateMutation();
   const [SchoolYearId, setSchoolYearId] = useState<string | undefined>();
-  const { currentData: StudentData, isFetching: isFetchingStudent } =
-    useStudentGetDataHasNoEnrollmentQuery({
-      search: searchStudent,
-      skip: 1,
-      take: 100,
-      schoolYearId: SchoolYearId,
-    });
-  const { currentData: stage, isFetching: isFetchingStage } =
-    useStageGetDataQuery();
-  const { currentData: SchoolYear, isFetching: isFetchingSchoolYear } =
-    useSchoolYearGetDataQuery();
-  const { currentData: Setting, isFetching: isFetchingSetting } =
-    useSettingGetDataQuery();
-  const handleSubmit = async (
-    values: FormValues,
-    { setSubmitting, resetForm }: FormikHelpers<FormValues>
-  ) => {
+  const { currentData: StudentData, isFetching: isFetchingStudent } = useStudentGetDataHasNoEnrollmentQuery({
+    search: searchStudent,
+    skip: 1,
+    take: 100,
+    schoolYearId: SchoolYearId,
+  });
+  const { currentData: stage, isFetching: isFetchingStage } = useStageGetDataQuery();
+  const { currentData: SchoolYear, isFetching: isFetchingSchoolYear } = useSchoolYearGetDataQuery();
+  const { currentData: Setting, isFetching: isFetchingSetting } = useSettingGetDataQuery();
+  const handleSubmit = async (values: FormValues, { setSubmitting, resetForm }: FormikHelpers<FormValues>) => {
     try {
       await StudentEnrollmentCreate({
         students: values.students
@@ -85,10 +70,7 @@ const PageComponent = () => {
         stageId: values.stageId,
       }).unwrap();
 
-      toast.success(
-        t(id ? "common.updated-successfully" : "common.added-successfully"),
-        { autoClose: 30000 }
-      );
+      toast.success(t(id ? "common.updated-successfully" : "common.added-successfully"), { autoClose: 30000 });
       resetForm();
       if (id) {
         router.back();
@@ -96,14 +78,8 @@ const PageComponent = () => {
     } catch (error: any) {
       console.error("Failed to operation :", error);
       if (error) {
-        if (
-          error.message ==
-          'Resource already exists. More details: {"modelName":"StudentEnrollment","target":"student_enrollments_schoolYearId_studentId_key"}'
-        ) {
-          return toast.error(
-            t("StudentEnrollmentPage.schoolYear-already-exists"),
-            { autoClose: 30000 }
-          );
+        if (error.message == 'Resource already exists. More details: {"modelName":"StudentEnrollment","target":"student_enrollments_schoolYearId_studentId_key"}') {
+          return toast.error(t("StudentEnrollmentPage.schoolYear-already-exists"), { autoClose: 30000 });
         }
         return toast.error(JSON.stringify(error), { autoClose: 30000 });
       }
@@ -135,13 +111,7 @@ const PageComponent = () => {
   return (
     <>
       <div className="mx-auto my-0 max-md:max-w-[100%] md:max-w-[50%]">
-        <BackButton
-          title={t(
-            id
-              ? "StudentEnrollmentPage.update-info"
-              : "StudentEnrollmentPage.add"
-          )}
-        />
+        <BackButton title={t(id ? "StudentEnrollmentPage.update-info" : "StudentEnrollmentPage.add")} />
 
         {isFetching || isFetchingSchoolYear || isFetchingSetting ? (
           <LoadingForm />
@@ -156,14 +126,11 @@ const PageComponent = () => {
               amountStudent: "0",
             }}
             validationSchema={studentSchema}
-            onSubmit={handleSubmit}
-          >
+            onSubmit={handleSubmit}>
             {(props: FormikProps<any>) => (
               <Form className={"px-4 flex flex-col gap-4"}>
                 <div className="Card flex flex-col gap-1">
-                  <div className=" text-base font-semibold text-black dark:text-white-dark  mb-2 ">
-                    {t("StudentEnrollmentPage.infoStudentEnrollment")}
-                  </div>
+                  <div className=" text-base font-semibold text-black dark:text-white-dark  mb-2 ">{t("StudentEnrollmentPage.infoStudentEnrollment")}</div>
 
                   <SelectForm
                     formikProps={props}
@@ -183,10 +150,7 @@ const PageComponent = () => {
                       isClearable: true,
                       onChange: (e) => {
                         setSchoolYearId((e as any)?.value ?? "");
-                        props.setFieldValue(
-                          "schoolYearId",
-                          (e as any)?.value ?? ""
-                        );
+                        props.setFieldValue("schoolYearId", (e as any)?.value ?? "");
                       },
                     }}
                   />
@@ -250,9 +214,7 @@ const PageComponent = () => {
                       stage
                         ? stage
                             .find((item) => item.id === props.values.stageId)
-                            ?.Class?.find(
-                              (item) => item.id === props.values.classId
-                            )
+                            ?.Class?.find((item) => item.id === props.values.classId)
                             ?.Section?.map((item) => {
                               return {
                                 label: t(item.name as any),
@@ -265,19 +227,14 @@ const PageComponent = () => {
                       isClearable: true,
                       isLoading: isFetchingStage,
                       onChange: (e) => {
-                        props.setFieldValue(
-                          "sectionId",
-                          (e as any)?.value ?? ""
-                        );
+                        props.setFieldValue("sectionId", (e as any)?.value ?? "");
                       },
                     }}
                   />
                 </div>
 
                 <div className="Card flex flex-col gap-2">
-                  <div className=" text-sm font-semibold text-black dark:text-white-dark  mb-2 ">
-                    {t("StudentEnrollmentPage.Students-and-Amount")}
-                  </div>
+                  <div className=" text-sm font-semibold text-black dark:text-white-dark  mb-2 ">{t("StudentEnrollmentPage.Students-and-Amount")}</div>
                   {/* <InputCurrencyMaskForm
                     formikProps={props}
                     name={"amountStudent"}
@@ -319,10 +276,7 @@ const PageComponent = () => {
                     ) : (
                       <div className="flex flex-col gap-1 py-2">
                         {StudentData?.data.map((item, index) => (
-                          <div
-                            className="flex gap-3 items-center"
-                            key={item.id}
-                          >
+                          <div className="flex gap-3 items-center" key={item.id}>
                             <CheckBoxForm
                               key={index}
                               formikProps={props}
@@ -330,31 +284,15 @@ const PageComponent = () => {
                               title={`${item.fullName}`}
                               props={{
                                 className: "rtl",
-                                checked: props.values.students.some(
-                                  (it: any) => it?.studentId == item.id
-                                ),
-                                value: props.values.students.some(
-                                  (it: any) => it?.studentId == item.id
-                                ),
+                                checked: props.values.students.some((it: any) => it?.studentId == item.id),
+                                value: props.values.students.some((it: any) => it?.studentId == item.id),
                                 onChange: (e) => {
                                   if (e.target.checked) {
-                                    props.setFieldValue(
-                                      `students.${index}.studentId`,
-                                      item.id
-                                    );
-                                    props.setFieldValue(
-                                      `students.${index}.amount`,
-                                      props.values.amountStudent
-                                    );
+                                    props.setFieldValue(`students.${index}.studentId`, item.id);
+                                    props.setFieldValue(`students.${index}.amount`, props.values.amountStudent);
                                   } else {
-                                    props.setFieldValue(
-                                      `students.${index}.studentId`,
-                                      ""
-                                    );
-                                    props.setFieldValue(
-                                      `students.${index}.amount`,
-                                      ""
-                                    );
+                                    props.setFieldValue(`students.${index}.studentId`, "");
+                                    props.setFieldValue(`students.${index}.amount`, "");
                                   }
                                 },
                               }}
